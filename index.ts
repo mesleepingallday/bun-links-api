96409b0b1d3882e63dddb051d074a6e7ccfc14b7
Hai Nguyen <hainguyenfe@outlook.com>
2024-10-08 15:10:11 +0700

    adding routes

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    const url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      const body = "Welcome to new world!";
+      return new Response(body);
+    }
+
+    if (url.pathname === "/api/bet") {
+      const body = "Bet API";
+      return new Response(body);
+    }
+
+    if (url.pathname === "/api/bet/create") {
+      const body = "Create Bet";
+      return new Response(body);
+    }
+
+    return new Response("404: Not Found");
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}`);
